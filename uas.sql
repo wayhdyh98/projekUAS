87b692d337839d17796c564e962783459f8d3b98
Wahyu Hidayah <37228903+Ahaha1998@users.noreply.github.com>
2018-12-02 23:26:32 +0700

    new uas

---
+++ uas.sql
@@ -0,0 +1,181 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 02, 2018 at 05:25 PM
+-- Server version: 10.1.28-MariaDB
+-- PHP Version: 7.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `uas`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `absensi`
+--
+
+CREATE TABLE `absensi` (
+  `nim` varchar(10) NOT NULL,
+  `alpha` int(2) NOT NULL DEFAULT '0',
+  `ijin` int(2) NOT NULL DEFAULT '0',
+  `sakit` int(2) NOT NULL DEFAULT '0',
+  `total_ais` int(2) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `absensi`
+--
+
+INSERT INTO `absensi` (`nim`, `alpha`, `ijin`, `sakit`, `total_ais`) VALUES
+('1741720002', 0, 0, 0, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `khs`
+--
+
+CREATE TABLE `khs` (
+  `idKhs` int(11) NOT NULL,
+  `kode_mt` varchar(3) NOT NULL,
+  `nim` varchar(10) NOT NULL,
+  `nilai` varchar(2) NOT NULL DEFAULT 'E'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `khs`
+--
+
+INSERT INTO `khs` (`idKhs`, `kode_mt`, `nim`, `nilai`) VALUES
+(1, 'BSD', '1741720002', 'C'),
+(2, 'DWB', '1741720002', 'A');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mahasiswa`
+--
+
+CREATE TABLE `mahasiswa` (
+  `nim` varchar(10) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `jurusan` varchar(20) NOT NULL,
+  `kelas` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `mahasiswa`
+--
+
+INSERT INTO `mahasiswa` (`nim`, `nama`, `password`, `jurusan`, `kelas`) VALUES
+('1741720002', 'Ahaha', 'ahaha', 'Teknik Informatika', '2C');
+
+--
+-- Triggers `mahasiswa`
+--
+DELIMITER $$
+CREATE TRIGGER `maba_absensi` AFTER INSERT ON `mahasiswa` FOR EACH ROW BEGIN
+    INSERT INTO absensi
+    set nim = new.nim; 
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_matkul`
+--
+
+CREATE TABLE `tb_matkul` (
+  `kode_mt` varchar(3) NOT NULL,
+  `matkul` varchar(50) NOT NULL,
+  `jurusan` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tb_matkul`
+--
+
+INSERT INTO `tb_matkul` (`kode_mt`, `matkul`, `jurusan`) VALUES
+('BSD', 'Basis Data', 'Teknik Informatika'),
+('DWB', 'Desan & Pemrograman Web', 'Teknik Informatika'),
+('JRK', 'Jaringan Komputer', 'Teknik Informatika'),
+('PBO', 'Pemrograman Berbasis Objek', 'Teknik Informatika');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `absensi`
+--
+ALTER TABLE `absensi`
+  ADD PRIMARY KEY (`nim`);
+
+--
+-- Indexes for table `khs`
+--
+ALTER TABLE `khs`
+  ADD PRIMARY KEY (`idKhs`),
+  ADD KEY `kode_mt` (`kode_mt`),
+  ADD KEY `nim` (`nim`);
+
+--
+-- Indexes for table `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  ADD PRIMARY KEY (`nim`);
+
+--
+-- Indexes for table `tb_matkul`
+--
+ALTER TABLE `tb_matkul`
+  ADD PRIMARY KEY (`kode_mt`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `khs`
+--
+ALTER TABLE `khs`
+  MODIFY `idKhs` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `absensi`
+--
+ALTER TABLE `absensi`
+  ADD CONSTRAINT `absensi_ibfk_1` FOREIGN KEY (`nim`) REFERENCES `mahasiswa` (`nim`);
+
+--
+-- Constraints for table `khs`
+--
+ALTER TABLE `khs`
+  ADD CONSTRAINT `khs_ibfk_1` FOREIGN KEY (`kode_mt`) REFERENCES `tb_matkul` (`kode_mt`),
+  ADD CONSTRAINT `khs_ibfk_2` FOREIGN KEY (`nim`) REFERENCES `mahasiswa` (`nim`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
